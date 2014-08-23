4f12d3ee67964cefb00458bec8750b4e63649275
Yecheng Fu <cofyc.jackson@gmail.com>
2014-08-23 18:09:57 +0800

    basic gmaps view

--- TracePath/ViewController.swift
+++ TracePath/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, GMSMapViewDelegate {
+
+    var gmaps: GMSMapView?
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        var target: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.6, longitude: 17.2)
+        var camera: GMSCameraPosition = GMSCameraPosition(target: target, zoom: 6, bearing: 0, viewingAngle: 0)
+
+        gmaps = GMSMapView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
+        if let map = gmaps? {
+            map.myLocationEnabled = true
+            map.camera = camera
+            map.delegate = self
+            self.view.addSubview(gmaps!)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
