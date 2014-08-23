9e57f2b1945094bb1b668c1c83b5d553086fb5f3
Yecheng Fu <cofyc.jackson@gmail.com>
2014-08-23 17:31:08 +0800

    add other googlemap required frameworks and bridge header

--- TracePath/AppDelegate.swift
+++ TracePath/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyCy7TNw8I1JcTsqX4II4tR_7JssPOinSKs")
         return true
     }
 
---
+++ TracePath/ObjcSwiftBridge.m
@@ -0,0 +1,9 @@
+//
+//  ObjcSwiftBridge.m
+//  TracePath
+//
+//  Created by Cofyc on 8/23/14.
+//
+//
+
+#import <Foundation/Foundation.h>
